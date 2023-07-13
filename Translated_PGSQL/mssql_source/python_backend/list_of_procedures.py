7665e3657efb26c665854b9984cded94b7bb97f2
pappala_naidu <papppala_naidu@persistent.com>
2023-07-13 07:57:12 +0000

    new commit

---
+++ Translated_PGSQL/mssql_source/python_backend/list_of_procedures.py
@@ -0,0 +1,7 @@
+python
+def get_all_procs():
+    procs_list = ['fnIsTestActive', 'fnEmailListUsesFeature', 'fnGetCustomerAttributeBannerContentBlockIdentifier', 'fnGetMailingLibraComponents']
+    return procs_list
+
+
+get_all_procs()
