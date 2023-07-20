632ab8531ebcc9411a4db11fb1fba08617cb0973
pappala_naidu <papppala_naidu@persistent.com>
2023-07-20 16:52:37 +0000

    new commit error files

---
+++ pgsql_error/mssql_source/python_backend/list_of_procedures.py
@@ -0,0 +1,5 @@
+python
+def get_all_procs():
+    procs_list = ['fnistestactive', 'fnemaillistusesfeature', 'fngetcustomerattributebannercontentblockidentifier', 'fngetmailinglibracomponents']
+    return procs_list
+
