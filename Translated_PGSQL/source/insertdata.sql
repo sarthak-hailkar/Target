dc01dbde982b809ff9932af8a83c317ab43368d7
pappala_naidu <papppala_naidu@persistent.com>
2023-08-01 10:11:33 +0000

    new commit

---
+++ Translated_PGSQL/source/insertdata.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee (id, emp_name, salary, hire_date)
+VALUES (1, 'John Smith', 5000, to_date('2022-01-01', 'YYYY-MM-DD'));
+
+INSERT INTO employee(id, emp_name, salary, hire_date)
+VALUES (2, 'Jane Doe', 6000, to_date('2021-03-15', 'YYYY-MM-DD'));
+
+INSERT INTO employee(id, emp_name, salary, hire_date)
+VALUES (3, 'David Johnson', 4500, to_date('2022-06-10', 'YYYY-MM-DD'));
+
+INSERT INTO employee (id, emp_name, salary, hire_date)
+VALUES (4, 'Sarah Williams', 5500, to_date('2023-01-25', 'YYYY-MM-DD'));
---
+++ Translated_PGSQL/source/sequence.sql
@@ -0,0 +1,8 @@
+CREATE SEQUENCE geeks_num
+AS INT
+START WITH 10
+INCREMENT BY 10;
+
+SELECT nextval('geeks_num');
+
+SELECT nextval('geeks_num');
