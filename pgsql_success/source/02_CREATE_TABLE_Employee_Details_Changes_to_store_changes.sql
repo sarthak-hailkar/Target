ed620a573a978bd86c7a9603dd35a573a0e253de
pappala_naidu <papppala_naidu@persistent.com>
2023-08-02 05:37:58 +0000

    commit for success files

---
+++ pgsql_success/source/02_CREATE_TABLE_Employee_Details_Changes_to_store_changes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "Employee_Details_Changes" (
+  "new_name" varchar(30),
+  "old_name" varchar(30),
+  "entry_date" varchar(30),
+  "operation"  varchar(30)
+);
---
+++ pgsql_success/source/08_UPDATE_Employee_Details.sql
@@ -0,0 +1,3 @@
+
+ UPDATE "Employee_Details" SET "emp_name" = 'Ranjit S' WHERE "emp_name" = 'Shreyas P';
+
